7ff08f2c0e4d006878bc6e5da4438870b80c1b02
SBMelissa <132725510+SBMelissa@users.noreply.github.com>
2023-05-21 19:16:58 -0400

    Primeros programas hechos del libro, pagina 29-34

---
+++ PPM/Programa 1.4/Programa 1.4/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+/* Aplicación de operadores; PROGRAMA 1.8 DEL LIBRO. */
+/*  Volumen y área del cilindroEl programa, al recibir como datos el radio y la altura de un cilindro,
+➥calcula su área y su volumen.RAD, ALT, VOL y ARE: variables de tipo real.   */
+
+void main(void)
+{
+    float RAD, ALT, VOL, ARE;
+    printf("Ingrese el radio y la altura del cilindro: ");
+    scanf("%f %f", &RAD, &ALT);
+
+    /*  M_PI es una constante definida en math.h que contiene el valor de PI */
+
+    VOL = M_PI * pow (RAD, 2) * ALT;
+    ARE = 2 * M_PI * RAD * ALT;
+    printf("\nEl volumen es: %6.2f  \t El area es: %6.2f", VOL, ARE);
+
+}
