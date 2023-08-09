7662f34f9f99885c6d61d177d8d2a2685947dcaf
SBMelissa <132725510+SBMelissa@users.noreply.github.com>
2023-08-09 17:35:38 -0400

    Act, Rixpert.

---
+++ PPM/Rispext-Piloto/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace Melissa2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            tabPage3 = new TabPage();
+            tabPage4 = new TabPage();
+            button2 = new Button();
+            dataGridView2 = new DataGridView();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            button3 = new Button();
+            button4 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            label5 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Controls.Add(tabPage4);
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(796, 447);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(dateTimePicker1);
+            tabPage1.Controls.Add(label5);
+            tabPage1.Controls.Add(textBox4);
+            tabPage1.Controls.Add(textBox3);
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(button4);
+            tabPage1.Controls.Add(button3);
+            tabPage1.Controls.Add(textBox2);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(dataGridView2);
+            tabPage1.Controls.Add(button2);
+            tabPage1.ForeColor = SystemColors.ControlText;
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(788, 419);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "FASE 1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(dataGridView1);
+            tabPage2.Controls.Add(label1);
+            tabPage2.Controls.Add(textBox1);
+            tabPage2.Controls.Add(button1);
+            tabPage2.ForeColor = SystemColors.ControlText;
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(788, 419);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "FASE 2";
+            tabPage2.UseVisualStyleBackColor = true;
+            tabPage2.Click += tabPage2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(8, 8);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(16, 16);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(298, 209);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(240, 150);
+            dataGridView1.TabIndex = 3;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(3);
+            tabPage3.Size = new Size(788, 419);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "FASE 3";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            tabPage4.Location = new Point(4, 24);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Padding = new Padding(3);
+            tabPage4.Size = new Size(788, 419);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "FASE 4";
+            tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Silver;
+            button2.FlatAppearance.BorderColor = Color.FromArgb(64, 64, 64);
+            button2.Font = new Font("Kristen ITC", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(67, 137);
+            button2.Name = "button2";
+            button2.Size = new Size(62, 29);
+            button2.TabIndex = 0;
+            button2.Text = "ADD";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(8, 181);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowTemplate.Height = 25;
+            dataGridView2.Size = new Size(774, 232);
+            dataGridView2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BorderStyle = BorderStyle.Fixed3D;
+            label2.Font = new Font("Leelawadee", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.DarkBlue;
+            label2.Location = new Point(3, 27);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 27);
+            label2.TabIndex = 2;
+            label2.Text = "NOMBRE:";
+            label2.Click += label2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(126, 29);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(137, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Silver;
+            button3.Font = new Font("Kristen ITC", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(174, 137);
+            button3.Name = "button3";
+            button3.Size = new Size(73, 29);
+            button3.TabIndex = 4;
+            button3.Text = "DELETE";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Silver;
+            button4.Font = new Font("Kristen ITC", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(280, 137);
+            button4.Name = "button4";
+            button4.Size = new Size(78, 26);
+            button4.TabIndex = 5;
+            button4.Text = "SAVE";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BorderStyle = BorderStyle.Fixed3D;
+            label3.Font = new Font("Kristen ITC", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(280, 27);
+            label3.Name = "label3";
+            label3.Size = new Size(122, 25);
+            label3.TabIndex = 6;
+            label3.Text = "ANALISTA:";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Kristen ITC", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.Black;
+            label4.Location = new Point(8, 73);
+            label4.Name = "label4";
+            label4.Size = new Size(93, 23);
+            label4.TabIndex = 7;
+            label4.Text = "ASESOR:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(418, 29);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(126, 75);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(137, 23);
+            textBox4.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Kristen ITC", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = Color.Black;
+            label5.Location = new Point(595, 29);
+            label5.Name = "label5";
+            label5.Size = new Size(82, 23);
+            label5.TabIndex = 11;
+            label5.Text = "FECHA:";
+            label5.Click += label5_Click;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(684, 29);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(91, 23);
+            dateTimePicker1.TabIndex = 12;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private DataGridView dataGridView1;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private TabPage tabPage3;
+        private TabPage tabPage4;
+        private Button button4;
+        private Button button3;
+        private TextBox textBox2;
+        private Label label2;
+        private DataGridView dataGridView2;
+        private Button button2;
+        private TextBox textBox3;
+        private Label label4;
+        private Label label3;
+        private TextBox textBox4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Label label5;
+        private DateTimePicker dateTimePicker1;
+    }
+}
---
+++ PPM/Rispext-Piloto/Form1.cs
@@ -0,0 +1,40 @@
+namespace Melissa2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
